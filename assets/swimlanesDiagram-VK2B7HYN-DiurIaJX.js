744616bf21c6921f0349100225bc37b21f125171
anxosanchez <anxosanchez@users.noreply.github.com>
2026-08-24 20:09:47 +0200

    Deploy usage statistics

--- assets/swimlanesDiagram-VK2B7HYN-DiurIaJX.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-A5DVABFB-CsSv0Yyg.js";import{_ as a}from"./mermaid.core-VwpqSC-X.js";import"./chunk-5VM5RSS4-eZnr6l0W.js";import"./chunk-XXDRQBXY-2hkK0BrC.js";import"./chunk-POPQ4Y6H-iRDtt27-.js";import"./chunk-F27PBJKO-Dr_qPUD6.js";import"./channel-7cHTPCxk.js";import"./index-DysWLD0n.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
