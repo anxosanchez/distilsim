6aff840d48ec75376687931b55ca6bf0a35ddb9c
anxosanchez <anxosanchez@users.noreply.github.com>
2026-08-24 19:35:53 +0200

    Deploy refined layout to GitHub Pages

---
+++ assets/channel-B9GAk4It.js
@@ -0,0 +1,1 @@
+import{ai as o,aj as n}from"./mermaid.core-T4id6DnT.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ assets/swimlanesDiagram-VK2B7HYN-DO80yuhm.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB-DsyL8Rwq.js";import{_ as a}from"./mermaid.core-T4id6DnT.js";import"./chunk-5VM5RSS4-Bth5mAIh.js";import"./chunk-XXDRQBXY-Dd1AzrO0.js";import"./chunk-POPQ4Y6H-M2LjTavc.js";import"./chunk-F27PBJKO-zu8_W7CN.js";import"./channel-B9GAk4It.js";import"./index-Bi2wsnLM.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
