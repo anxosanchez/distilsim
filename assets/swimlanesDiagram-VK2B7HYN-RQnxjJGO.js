6ac4af4af00a3c9d03970820bfff57d02e426304
anxosanchez <anxosanchez@users.noreply.github.com>
2026-08-24 20:11:39 +0200

    Deploy 3D label typography

---
+++ assets/swimlanesDiagram-VK2B7HYN-RQnxjJGO.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB-Biy0gTZ1.js";import{_ as a}from"./mermaid.core-BKcyp5UU.js";import"./chunk-5VM5RSS4-BZW05Zb-.js";import"./chunk-XXDRQBXY-01WxdKok.js";import"./chunk-POPQ4Y6H-jm519gpw.js";import"./chunk-F27PBJKO-BcB3k3Zg.js";import"./channel-B6UHoe1n.js";import"./index-DjyKAtMW.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
--- assets/swimlanesDiagram-VK2B7HYN-dtZmVCUx.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-A5DVABFB-C1EfZ32o.js";import{_ as a}from"./mermaid.core-CnGUjLyx.js";import"./chunk-5VM5RSS4-BP6wjq-d.js";import"./chunk-XXDRQBXY-Dz_hET7A.js";import"./chunk-POPQ4Y6H-LIjzrJQy.js";import"./chunk-F27PBJKO-B0hkaHSN.js";import"./channel-DXIaxLdK.js";import"./index-Bc1he7Ap.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
