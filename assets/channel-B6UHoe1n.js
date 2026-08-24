2b62b149e0fae8c7ca04e52cfc70eeffbe12dc1f
anxosanchez <anxosanchez@users.noreply.github.com>
2026-08-24 20:14:19 +0200

    Deploy extended usage statistics

--- assets/channel-B6UHoe1n.js
+++
@@ -1,1 +0,0 @@
-import{ai as o,aj as n}from"./mermaid.core-BKcyp5UU.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ assets/swimlanesDiagram-VK2B7HYN-CS-KnuRP.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB-XYjMt_sO.js";import{_ as a}from"./mermaid.core-BXD69scu.js";import"./chunk-5VM5RSS4-B16SfrBJ.js";import"./chunk-XXDRQBXY-B_VElIhs.js";import"./chunk-POPQ4Y6H-ZtNfeKZ_.js";import"./chunk-F27PBJKO-CCbMB43c.js";import"./channel-CXQOE9ke.js";import"./index-KzWNz0db.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
