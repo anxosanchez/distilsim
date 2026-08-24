08fa69ce55d3b12d57aa913f19572e551f452023
anxosanchez <anxosanchez@users.noreply.github.com>
2026-08-24 22:07:46 +0200

    Deploy 3D label size

---
+++ assets/swimlanesDiagram-VK2B7HYN-gkhO13QV.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB-CkrlnyB_.js";import{_ as a}from"./mermaid.core-DF-2MDnn.js";import"./chunk-5VM5RSS4-BNf9Jt56.js";import"./chunk-XXDRQBXY-Crq6B9IL.js";import"./chunk-POPQ4Y6H-miU-GY_y.js";import"./chunk-F27PBJKO-BjLD3XRj.js";import"./channel-i0BgBXxG.js";import"./index-BP0dRFDq.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
