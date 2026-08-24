e2f2756c6876c5b737a5d2a72a7b908fd6722037
anxosanchez <anxosanchez@users.noreply.github.com>
2026-08-24 19:49:37 +0200

    Deploy responsive header

--- assets/swimlanesDiagram-VK2B7HYN-Y90zYq41.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-A5DVABFB-D92dfNql.js";import{_ as a}from"./mermaid.core-5A1UmPQ2.js";import"./chunk-5VM5RSS4-C5p9tLWC.js";import"./chunk-XXDRQBXY-DUBoWYqi.js";import"./chunk-POPQ4Y6H-CqX7-BsV.js";import"./chunk-F27PBJKO-B3IRCJ8I.js";import"./channel-HVFLJ4KL.js";import"./index-wLvpNpLX.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
