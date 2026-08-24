1f065f3a86093db67a60e53e9c2a49892ed8f3ee
anxosanchez <anxosanchez@users.noreply.github.com>
2026-08-24 19:29:04 +0200

    Deploy to GitHub Pages (build GH_PAGES=1)

---
+++ assets/swimlanesDiagram-VK2B7HYN-BVl3brmw.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB-Zeh_xPJI.js";import{_ as a}from"./mermaid.core-DuMl1743.js";import"./chunk-5VM5RSS4-D4BouTUs.js";import"./chunk-XXDRQBXY-DyXsT3MD.js";import"./chunk-POPQ4Y6H-DBzT_gQa.js";import"./chunk-F27PBJKO-C2iNpvKt.js";import"./channel-DElec_mM.js";import"./index-DA7-GPnU.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
